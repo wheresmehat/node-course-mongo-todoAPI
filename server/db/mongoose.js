e75bdc92ceccf93eed85bd59e379afbe1bdd2f32
wheresmehat <csstogo@gmail.com>
2017-02-03 18:14:41 +0100

    Changed back mongoose

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ var mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp" || "mongodb://simao:lozinka25@ds139969.mlab.com:39969/todoappdb-api");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://simao:lozinka25@ds139969.mlab.com:39969/todoappdb-api"  || "mongodb://localhost:27017/TodoApp");
 
 module.exports = { mongoose };
