1e01baaaf0715c9bb94b8b860a744c32d825e4e4
wheresmehat <csstogo@gmail.com>
2017-02-10 15:24:15 +0100

    JWT and token in header

--- server/server.js
+++ server/server.js
@@ -151,6 +151,38 @@ app.patch("/todos/:id", (req, res) => {
 });
 
 
+app.post("/users", (req, res) => {
+
+    var body = _.pick(req.body, ["email", "password"]);
+    
+    var user = new User(body);
+
+    user.save()
+    .then(() => {
+
+        return user.generateAuthToken();
+       
+    })
+    .then((token) => {
+
+        res.header("x-auth", token).send(user);
+    })
+    .catch((err) => {
+
+        if (err.code === 11000) {
+
+            res.status(400).send({message: "email already in use"});
+        }
+        else {
+            
+            res.status(400).send(err);
+        }
+        
+    });
+
+});
+
+
 app.listen(port, () => {
 
     console.log(`Started up at port ${port}`);
